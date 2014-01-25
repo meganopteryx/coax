abce3d9981a4343f127e4ff8fa4073bb17b42a81
Megan Bednarz <megan_bednarz@yahoo.com>
2014-01-25 11:06:40 -0600

    Added rigidbody to other entity, added intial random motion

---
+++ CoaxUnityProj/Assets/Scripts/StrangerBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrangerBehavior : MonoBehaviour {
+	
+	public float maxVelocity = 50;
+	
+	// Use this for initialization
+	void Start () {
+		if(rigidbody){
+			//rigidbody.velocity.Set(Random.Range (0,maxVelocity),Random.Range (0,maxVelocity),0);
+			rigidbody.AddForce(new Vector3(Random.Range (-maxVelocity,maxVelocity),Random.Range (-maxVelocity,maxVelocity),0));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
