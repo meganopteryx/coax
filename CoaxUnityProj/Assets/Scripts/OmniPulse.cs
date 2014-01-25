380a6b3a28f9f10e89607ca0a171ee46c97f7009
friken <temp123>
2014-01-25 16:17:40 -0600

    Music and fx 1.0

--- CoaxUnityProj/Assets/Scripts/OmniPulse.cs
+++ CoaxUnityProj/Assets/Scripts/OmniPulse.cs
@@ -9,12 +9,14 @@ public class OmniPulse : MonoBehaviour {
     float pulseFadeTime = 0.01f;
     Transform player;
     GameObject pulse;
+    AudioClip sndPulse;
 
 
 	// Use this for initialization
 	void Start () 
     {
         player = GameObject.Find("Player").transform;
+        sndPulse = Resources.Load("sndPulse") as AudioClip;
         StartCoroutine(coPulse());
 	}
 	
@@ -28,11 +30,17 @@ public class OmniPulse : MonoBehaviour {
     {
         while (1 != 0) //ForEvaAndaDay
         {
+            //Make A Pulse
             pulse = Instantiate(omniPrefab) as GameObject;
             pulse.transform.position = player.position;
             pulse.transform.rotation = player.rotation;
+
+            //Set Initial Color
             Color color = pulse.renderer.material.GetColor("_TintColor");
             color.r = 0.5f; color.g = 0.5f; color.b = 0.5f; color.a = 0.5f;
+
+            //PlaySound
+            audio.PlayOneShot(sndPulse);
 
             //Scale-Fade
             for (int i = 0; i < 100; i++)
