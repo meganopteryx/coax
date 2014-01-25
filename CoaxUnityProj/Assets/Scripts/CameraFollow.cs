81f4d160e14d4a1a003ffb7ba0a2709b5d477249
friken <temp123>
2014-01-25 12:40:49 -0600

    PlayerMovement
    and camfollow shell

---
+++ CoaxUnityProj/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public float camZoomLerp = 2.0f;
+    public float camMoveLerp = 2.0f;
+    public float maxZoomIn = 10.0f;
+    public float maxZoomOut = 10.0f;
+    
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        CamFollow();
+	}
+
+    void CamFollow()
+    {
+
+    }
+}
--- CoaxUnityProj/Assets/Scripts/Player.cs
+++ CoaxUnityProj/Assets/Scripts/Player.cs
@@ -4,10 +4,56 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+    public enum ControlScheme {MOUSE_WSDF, CONTROLLER};
+    public ControlScheme controlScheme = ControlScheme.MOUSE_WSDF;
+    public float maxTurnSpeed = 10;
+    public float maxSpeed = 100;
+    public float thrust = 15;
+    
+
+    void Start () 
+    {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
+        //Mouse-WSDF Controll Scheme
+        switch (controlScheme)
+        {
+            case ControlScheme.CONTROLLER:
+                break;
+            default: //Mouse-WSDF default
+                FollowMouse();
+                WSDF();
+                break;
+        }   
 	}
+
+    void FollowMouse()
+    {
+        //get mouse and object screen pos
+        Vector3 mouse_pos = Input.mousePosition;
+        Vector3 object_pos = Camera.main.WorldToScreenPoint(transform.position);
+
+        //ATan2
+        mouse_pos.x = mouse_pos.x - object_pos.x;
+        mouse_pos.y = mouse_pos.y - object_pos.y;
+        float angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+
+        //Set Rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(new Vector3(0,0,angle-90)), Time.deltaTime * (maxTurnSpeed/2));
+    }
+
+    void WSDF()
+    {
+        //X-Axis
+        rigidbody.AddForce(Input.GetAxis("Horizontal") * thrust, 0, 0);
+        //Y-Axis
+        rigidbody.AddForce(0, Input.GetAxis("Vertical") * thrust, 0);
+
+        //Speed Limit
+        if (rigidbody.velocity.magnitude > 0)
+            rigidbody.velocity = rigidbody.velocity / rigidbody.velocity.magnitude * Mathf.Min(rigidbody.velocity.magnitude, maxSpeed);
+    }
 }
